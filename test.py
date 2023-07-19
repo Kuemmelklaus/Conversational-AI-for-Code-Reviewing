17ceb29e70cc5e6f59cdbbd398d506fe92629b6e
Kuemmelklaus <leon.fischer1208@googlemail.com>
2023-07-19 13:52:09 +0200

    improved prompt + testing

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ response = openai.ChatCompletion.create(
     n = 1,
     messages = [
         {"role": "system", "content": "Your name is 'ABAP-Linter' and your purpose is to assist development in the ABAP language."},
-        {"role": "user", "content": "Always answer in a systematic scheme. This has to includes the first and last line, a grading from 0 to 10 and a reason for that given grade."},
+        {"role": "user", "content": "Always answer in a systematic scheme. This has to includes a grading of a specific section of code from 0 to 10, the first and last line where this graded code is written and a reason for that given grade."},
         {"role": "assistant", "content": 'I will answer prompts in the following format: \n"lineFrom": 25, \n"lineTo": 49, \n"grade": 4, \n"reason": "This function is named "add" although it performs an arithmetic multiplication. Please rename the function."'},
         {"role": "user", "content": promtText}
     ]
@@ -33,7 +33,7 @@ response = openai.ChatCompletion.create(
 
 for choices in response["choices"]:
     print(choices.message.content)
-    print("==================")
+    print("===========================")
 
 # print(response.choices[0].message.content)
 print(response)
