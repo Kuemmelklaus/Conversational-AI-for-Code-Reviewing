61e5dfc7bb3768837d2c4d20bcc3e7bfbd4a567f
Kuemmelklaus <leon.fischer1208@googlemail.com>
2023-08-07 16:55:30 +0200

    update /docs

--- app.py
+++ app.py
@@ -22,12 +22,49 @@ def health():
 class Lin(Schema):
     programmingLanguage = String(
         required = True,
-        metadata = {"title": "programming language", "example": "Python"}
+        metadata = {"title": "Programming language", "example": "Python"}
     )
     code = String(
         required = True,
-        metadata = {"title": "code to be reviewed", "example": 'print("Hello World!")'}
+        metadata = {"title": "Code to be reviewed", "example": 'print("Hello World!")'}
     )
+
+class Example_Response(Schema):
+    code = String(
+        metadata = {"title": "Code to be reviewed", "example": 'print("Hello World!")'}
+    )
+    completion_tokens = Integer(
+        metadata = {"title": "Tokens used to create the response", "example": 420}
+    )
+    date = String(
+        metadata = {"title": "Time when the response was created", "example": "2023-08-01T11:30:22.514049"}
+    )
+    model = String(
+        metadata = {"title": "The name of the model which created the response", "example": "gpt-3.5-turbo-16k"}
+    )
+    programmingLanguage = String(
+        metadata = {"title": "Programming language", "example": "Python"}
+    )
+    success = Boolean(
+        required = True,
+        metadata = {"title": "Confirms that the response is valid", "example": True}
+    )
+    total_tokens = Integer(
+        metadata = {"title": "Tokens used which contains the request and the response", "example": 2696}
+    )
+    lint = Field(
+        metadata = {"title": "Contains a list of review messages", "example": ""}
+    )
+#    [{
+#        lineFrom = Integer(example = 1),
+#        "lineTo": 2,
+#        "message": "The 'except' block in the 'submit' function catches all exceptions without specifying which exceptions to catch. It is generally recommended to catch specific exceptions rather than catching all exceptions, as it can make it harder to debug and handle specific errors."
+#    },
+#    {
+#        "lineFrom": 12,
+#        "lineTo": 12,
+#        "message": "There is a trailing comma in the list. It is not necessary and can be removed."
+#    }]
 
 @app.post("/linter")
 @app.input(Lin, location = "json")
