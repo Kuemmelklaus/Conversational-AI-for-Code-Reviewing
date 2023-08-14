89ae132c6a0d9adceb8fed99ed42be1350377f90
Kuemmelklaus <leon.fischer1208@googlemail.com>
2023-08-14 14:54:52 +0200

    implementing fetch request

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -7,68 +7,65 @@ const CodeEditor = ({ lang }) => {
   const [language, setLanguage] = useState(null);
   const [theme, setTheme] = useState("vs-dark");
 
-  setLanguage(lang);
+  useEffect(() => {
+    setLanguage(lang);
+  }, []);
 
   const handleEditorChange = (data) => {
     setCode(data);
     console.log(data, code);
   };
 
+  function ReviewButton() {
+    function sendPostRequest(code, language) {
+      console.log(code, language);
+      var jsonData = {
+        programmingLanguage: language,
+        code: code,
+      };
+      console.log(jsonData);
+
+      const fetchRequest = async () => {
+        const response = await fetch("http://127.0.0.1:5000/linter", {
+          method: "POST",
+          headers: new Headers({ "content-type": "application/json" }),
+          body: JSON.stringify(jsonData),
+        });
+        const data = await response.json();
+        console.log(data);
+      };
+
+      return fetchRequest();
+    }
+    return (
+      <button onClick={() => sendPostRequest(code, language)}>
+        Send review request
+      </button>
+    );
+  }
+
   return (
-    <Editor
-      height={"70vh"}
-      width={`100%`}
-      value={code}
-      language={language}
-      theme={theme}
-      onChange={handleEditorChange}
-    />
+    <div>
+      <Editor
+        height={"70vh"}
+        width={`100%`}
+        value={code}
+        language={language}
+        theme={theme}
+        onChange={handleEditorChange}
+      />
+      <p>
+        <br />
+      </p>
+      <ReviewButton />
+    </div>
   );
 };
 
 function App() {
   const [healthStatus, setHealthStatus] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [code, setCode] = useState(null);
   const [language, setLanguage] = useState("python");
-
-  /*
-  const onChange = (action, data) => {
-    switch (action) {
-      case "code": {
-        setCode(data);
-        console.log(code);
-        break;
-      }
-      default: {
-        console.warn("case not handled!", action, data);
-      }
-    }
-  };
-
-  const CodeEditorWindow = ({ onChange, language, code, theme }) => {
-    const [value, setValue] = useState(code || "");
-
-    const handleEditorChange = (value) => {
-      setValue(value);
-      onChange("code", value);
-    };
-
-    return (
-      <div className="input-window">
-        <Editor
-          height="70vh"
-          width={`100%`}
-          language={language || "python"}
-          value={value}
-          theme={theme}
-          defaultValue={"" || "# some comment"}
-          onChange={handleEditorChange}
-        />
-      </div>
-    );
-  };
-  */
 
   useEffect(() => {
     const getHealth = async () => {
@@ -92,42 +89,10 @@ function App() {
       <div className="App-body">
         <div className="code-editor">
           <CodeEditor lang={language} />
-          <p>
-            <br />
-          </p>
-          <ReviewButton />
         </div>
       </div>
     </div>
   );
-}
-
-function ReviewButton() {
-  return (
-    <button onClick={() => sendPostRequest(Editor.code, Editor.language)}>
-      Send review request
-    </button>
-  );
-}
-
-function sendPostRequest(code, language) {
-  var jsonData = {
-    programmingLanguage: language,
-    code: code,
-  };
-  console.log(jsonData);
-  const fetchRequest = async () => {
-    const response = await fetch("http://127.0.0.1:5000/linter", {
-      method: "POST",
-      headers: new Headers({ "content-type": "application/json" }),
-      body: JSON.stringify(jsonData),
-    });
-    const data = await response.json();
-    console.log(data);
-  };
-
-  console.log("AAA");
-  return fetchRequest();
 }
 
 export default App;
